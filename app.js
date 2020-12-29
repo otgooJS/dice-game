57272cd37e14a056b65a94a75abd2460b60990b5
Otg <otgonjargal.natsag@gmail.com>
2020-12-29 18:56:09 +0800

    activePlayer status wlll be changed

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч, 1-р тоглогчийг 0, 2-р тоглогчийг 1 гэе.
-var activePlayer = 1;
+var activePlayer = 0;
 
 //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var score = [0, 0];
@@ -20,7 +20,7 @@ var diceNumber = Math.floor(Math.random() * 6) + 1;
 //document.querySelector(".dice").style.display = "none"; //JS Lesson003.41_37.00: making dice png invisible. ''block' geheer visible
 
 //document.querySelector("#score-0").textContent = 0;
-//document.getElementById("#score-0").textContent = 0; // ene ni ilyy hyrdan ajillana!!!! ID-gaar ni elements-iig haij bgaa tohgioldold get-iig should use. 03.042_25.20
+//document.getElementById("score-0").textContent = 0; // ene ni ilyy hyrdan ajillana!!!! ID-gaar ni elements-iig haij bgaa tohgioldold get-iig should use. 03.042_25.20
 // <div class="player-score" id="score-0">43</div>
 
 document.getElementById("score-0").textContent = "0"; // 03.042_25.26: Text content ryy string uguh ystoi!!
@@ -30,15 +30,37 @@ document.getElementById("current-1").textContent = "0";
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+
 //btn-roll: class ychiraas query-iig ashigna!!!! 03.042_29.10
 document.querySelector(".btn-roll").addEventListener("click", function () {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
   diceDom.style.display = "block"; // if none, invisible, block is visible
   diceDom.src = "dice-" + diceNumber + ".png";
-}); // 04.042_32.30 Anonymous function used here coz roll dice btn is only one.
-
-/*function shooShid() {
+  /*function shooShid() {
   var diceNumber = Math.floor(Math.random() * 6) + 1;
   alert("Шоо буулаа:" + diceNumber);
 }
 */
+
+  //03.043 Улаан хэсэгт оноо хураах, Тоглогчийн ээлжийг солих,  2020.12.29
+  //sum score in red box, Change players round.
+  // byysan too 1-ees yalgaatai bol eeljiin onoog +dyylne
+
+  if (diceNumber !== 1) {
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //Onoog tegleed toglogchiin eeljiig solih
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = "0";
+    //If active player equials to 0, make it 1. If not, active player=1
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    //ActiveIcon must be moved: Red point
+    document.querySelector(".player-0-panel").classList.toggle("active"); //03.046_29.00: toggle: bval remove
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    //shoog tyr alga bolgoh
+    diceDom.style.display = "none";
+  }
+}); // 04.042_32.30 Anonymous function used here coz roll dice btn is only one.
